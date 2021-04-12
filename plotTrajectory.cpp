177b47ea8b179e48b449adc02e86964d6a3bf26a
刘祥辰 <2755939369@qq.com>
2021-04-13 07:50:05 +0800

    first commit by AlexLiu

---
+++ plotTrajectory.cpp
@@ -0,0 +1,119 @@
+#include <pangolin/pangolin.h>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <unistd.h>
+#include<fstream> 
+#include<iostream>
+#include<sstream>
+#include<string> 
+#include<opencv2/opencv.hpp>
+
+using namespace std;
+using namespace Eigen;
+
+std::string trajectory_file = "/home/alexliu/AlexLiu_Learning_Slam/slambook2-master/ch3/examples/KeyFrameTrajectory.txt";
+const string &stringPath = "/home/alexliu/AlexLiu_Learning_Slam/slambook2-master/ch3/examples/EuRoC.yaml";
+void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>>);
+float mViewpointX, mViewpointY, mViewpointZ, mViewpointF;
+
+int main(int argc, char **argv) {
+
+  vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>> poses;
+  ifstream fin(trajectory_file);
+  std::string line;
+  cv::FileStorage fSettings(stringPath, cv::FileStorage::READ);
+  cv::FileNode node = fSettings["Viewer.ViewpointX"];
+    if(!node.empty())
+    {
+        mViewpointX = node.real();
+    }
+
+    node = fSettings["Viewer.ViewpointY"];
+    if(!node.empty())
+    {
+        mViewpointY = node.real();
+    }
+
+    node = fSettings["Viewer.ViewpointZ"];
+    if(!node.empty())
+    {
+        mViewpointZ = node.real();
+    }
+
+    node = fSettings["Viewer.ViewpointF"];
+    if(!node.empty())
+    {
+        mViewpointF = node.real();
+    }
+  while (!fin.eof())
+  {
+  if (!fin) {
+    cout << "cannot find trajectory file at " << trajectory_file << endl;
+    return 1;
+  }
+  
+   double time, tx, ty, tz, qx, qy, qz, qw;
+    fin >> time >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
+    Isometry3d Twr(Quaterniond(qw, qx, qy, qz));
+    Twr.pretranslate(Vector3d(tx, ty, tz));
+    poses.push_back(Twr);
+  }
+  fin.close();
+  DrawTrajectory(poses);
+  
+  cout << "read total " << poses.size() << " pose entries" << endl;
+  return 0;
+}
+
+/*******************************************************************************************/
+void DrawTrajectory(vector<Isometry3d, Eigen::aligned_allocator<Isometry3d>> poses) {
+  // create pangolin window and plot the trajectory
+  pangolin::CreateWindowAndBind("Trajectory Viewer", 1024, 768);
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  pangolin::OpenGlRenderState s_cam(
+    pangolin::ProjectionMatrix(1024, 768, mViewpointF,mViewpointF, 512, 389, 0.1, 1000),
+    pangolin::ModelViewLookAt(mViewpointX,mViewpointY,mViewpointZ, 0, 0, 0, 0.0, -1.0, 0.0)
+  );
+
+  pangolin::View &d_cam = pangolin::CreateDisplay()
+    .SetBounds(0.0, 1.0, 0.0, 1.0, -1024.0f / 768.0f)
+    .SetHandler(new pangolin::Handler3D(s_cam));
+    while (!pangolin::ShouldQuit())
+    {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    d_cam.Activate(s_cam);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glLineWidth(2);
+    for (size_t i = 0; i < poses.size()-1; i++) {
+      // 画每个位姿的三个坐标轴
+      auto p1 = poses[i],p2 = poses[i+1];
+      Vector3d Ow = poses[i].translation();
+      Vector3d Xw = poses[i] * (0.1 * Vector3d(1, 0, 0));
+      Vector3d Yw = poses[i] * (0.1 * Vector3d(0, 1, 0));
+      Vector3d Zw = poses[i] * (0.1 * Vector3d(0, 0, 1));
+      glBegin(GL_LINES);
+      glColor3f(1.0, 0.0, 0.0);
+      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glVertex3d(Xw[0], Xw[1], Xw[2]);
+      glColor3f(0.0, 1.0, 0.0);
+      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glVertex3d(Yw[0], Yw[1], Yw[2]);
+      glColor3f(0.0, 0.0, 1.0);
+      glVertex3d(Ow[0], Ow[1], Ow[2]);
+      glVertex3d(Zw[0], Zw[1], Zw[2]);
+      glEnd();
+
+      glColor3f(0.0,0.0,0.0);
+      glBegin(GL_LINES);
+      glVertex3d(p1.translation()[0],p1.translation()[1],p1.translation()[2]);
+      glVertex3d(p2.translation()[0], p2.translation()[1], p2.translation()[2]);
+      glEnd();
+      pangolin::FinishFrame();
+      
+    } 
+    }
+    // sleep(1);   // sleep 5 ms
+}
